7ed273c233cf4bb64dcd0065c9d3f2646490d50b
João Mota <jdmota@outlook.pt>
2017-05-04 14:29:54 +0100

    Maybe private?

--- 4U/src/game/Checker.java
+++ 4U/src/game/Checker.java
@@ -12,7 +12,7 @@ public class Checker {
 		return checkHorizontaly(p) && checkVerticaly(p) && checkDiagonaly(p) && checkOtherDiagonaly(p);
 	}
 	
-	public boolean checkHorizontaly(Piece p){
+	private boolean checkHorizontaly(Piece p){
 		int col = p.getCol();
 		int row = p.getRow();
 		
@@ -30,7 +30,7 @@ public class Checker {
 		return (countLeft + countRight + 1) >= 4;
 	}
 	
-	public boolean checkVerticaly(Piece p){
+	private boolean checkVerticaly(Piece p){
 		int col = p.getCol();
 		int row = p.getRow();
 		
@@ -43,7 +43,7 @@ public class Checker {
 		return (countDown + 1) >= 4;
 	}
 	
-	public boolean checkDiagonaly(Piece p){
+	private boolean checkDiagonaly(Piece p){
 		int col = p.getCol();
 		int row = p.getRow();
 		
@@ -61,7 +61,7 @@ public class Checker {
 		return (countDown + countUp + 1) >= 4;
 	}
 	
-	public boolean checkOtherDiagonaly(Piece p){
+	private boolean checkOtherDiagonaly(Piece p){
 		int col = p.getCol();
 		int row = p.getRow();
 		
